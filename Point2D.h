f2a2fa8bd1bdfd6cf586a533f99727e757bdb54d
LALBTOR1 <lalbtor1@alumno.upv.es@ldsic-vdi17.upvnet.upv.es>
2023-10-07 18:20:48 +0200

    Actualizados y subidos todos los archivos relacionados a Point2D

--- Point2D.h
+++ Point2D.h
@@ -12,32 +12,14 @@ class Point2D{
         double x;
         double y;
 
-        Point2D(double x=0, double y=0){
-            this->x = x;
-            this->y = y;
-        } 
+        Point2D(double x=0, double y=0);
 
-        static double distance(const Point2D &a, const Point2D &b){
-            double distance = sqrt(pow(a.x-b.x,2.0)+pow(a.y-b.y,2.0));
-            return distance;
-        } 
+        static double distance(const Point2D &a, const Point2D &b); 
 
         friend bool operator==(const Point2D &a, const Point2D &b);
         friend bool operator!=(const Point2D &a, const Point2D &b);
         friend ostream& operator<<(std::ostream &out, const Point2D &p);
 };
 
-bool operator==(const Point2D &a, const Point2D &b){
-    return a.x == b.x && a.y == b.y;
-} 
-
-bool operator!=(const Point2D &a, const Point2D &b){
-    return a.x != b.x || a.y != b.y; 
-} 
-
-ostream& operator<<(std::ostream &out, const Point2D &p){
-    out << "(" << p.x << "," << p.y << ")";
-    return out;
-} 
 
 #endif
