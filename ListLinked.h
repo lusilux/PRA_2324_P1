c7507bbc2b1b0a96e2113327f7a0edc8fa1a76b8
LALBTOR1 <lalbtor1@alumno.upv.es@ldsic-vdi17.upvnet.upv.es>
2023-10-07 17:41:52 +0200

    Añadido código de test de la clase ListLinked; Makefile actualizado

--- ListLinked.h
+++ ListLinked.h
@@ -24,7 +24,7 @@ template <typename T> class ListLinked : public List<T> {
             if (pos >= 0 && pos < n) { 
                 Node<T> *aux = first;
                 for(int i = 0; i < pos; i++)aux = aux->next;
-                return aux;
+                return aux->data;
              }
 			else { throw out_of_range("Posición no válida del array\n"); }
         } 
@@ -32,14 +32,15 @@ template <typename T> class ListLinked : public List<T> {
         friend std::ostream& operator<<(std::ostream &out, const ListLinked<T> &list){
             Node<T> *aux = list.first;
             for(int i = 0; i < list.n; i++){
-                out << aux.data << " ";
+                out << aux->data << " "; //Cuidado con esto
                 aux = aux->next; 
             } 
 			return out;
         } 
 		void insert(int pos, T e) override{
+            if (pos < 0 || pos > n) { throw out_of_range("Posición no válida del array\n"); }
             if (pos == 0) { 
-                first = new Node<T>(e.data, first);
+                first = new Node<T>(e, first);
             } else {
                 Node<T>* prev = first;
                 int i = 0;
@@ -48,29 +49,35 @@ template <typename T> class ListLinked : public List<T> {
                     i++;
                 }
                 if (prev != nullptr) {
-                    prev->next = new Node<T>(e.data, prev->next);
+                    prev->next = new Node<T>(e, prev->next);
                 } 
             }
+            n++;
         } 
 		void append(T e) override{insert(n,e);} 
 		void prepend(T e) override{insert(0,e);} 
 		T remove(int pos) override{
             if (pos < 0 || pos >= n) { throw out_of_range("Posición no válida del array\n"); }
             else{
+                T x;
                 if(pos == 0){
                     Node<T>* aux = first;
+                    x = aux->data;
                     first = first->next;
                     delete aux;
                 }else{
                     Node<T>* aux = first->next;
                     Node<T>* prev = first;
-                    for(int i = 1; i <= pos; i++){
+                    for(int i = 1; i < pos; i++){
                         aux = aux->next;
                         prev = prev->next;
                     } 
+                    x = aux->data;
                     prev->next = aux->next;
                     delete aux;
                 } 
+                n--;
+                return x;
             } 
         } 
 		T get(int pos) override{
@@ -80,14 +87,14 @@ template <typename T> class ListLinked : public List<T> {
                 for(int i = 0; i < pos; i++){
                     aux = aux->next;
                 }
-                return aux; 
+                return aux->data; 
             }  
         } 
 		int search(T e) override{
 
             Node<T>* aux = first; 
             int i = 0;
-            while (aux != nullptr && aux != e) { 
+            while (aux != nullptr && aux->data != e) { 
                 aux = aux->next;
                 i++;
             }
